59c17f853b9d889db4ad35f836ac2b6c9611718b
banksolo2 <seunolo2@gmail.com>
2026-04-20 13:04:20 +0100

    add SpringBoot setup

---
+++ src/main/java/app/web/seunolo2/banksshop/BanksShopApplication.java
@@ -0,0 +1,13 @@
+package app.web.seunolo2.banksshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanksShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/web/seunolo2/banksshop/BanksShopApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.seunolo2.banksshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
